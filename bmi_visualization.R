84dd2e6db51278cdfc7fe8d7aa3638bac1301d3e
ella <umbr.ella0991@gmail.com>
2019-08-18 16:53:54 +0900

    add to view average data

--- bmi_visualization.R
+++ bmi_visualization.R
@@ -5,6 +5,7 @@ bmi <- read.csv("bmi.csv")
 
 # 2. 나이별 bmi 평균 구하기
 bmi_mean <- aggregate(bmi~age, bmi, mean)
+View(bmi_mean)
 
 # 3. bmi 평균 데이터로 라인 그래프 그리기
 ggplot(data=bmi_mean, aes(x=age, y=bmi)) + geom_line()
