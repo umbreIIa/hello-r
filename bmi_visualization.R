b911945356029c0c9b82b475b82b73ce7be17f99
ella <umbr.ella0991@gmail.com>
2019-08-18 16:52:25 +0900

    add the explanation

--- bmi_visualization.R
+++ bmi_visualization.R
@@ -1,5 +1,10 @@
+### bmi 데이터로 라인 그래프 그리기
+
+# 1. bmi 데이터 불러오기
 bmi <- read.csv("bmi.csv")
 
+# 2. 나이별 bmi 평균 구하기
 bmi_mean <- aggregate(bmi~age, bmi, mean)
 
-ggplot(data=bmi_mean, aes(x=age, y=avg(bmi))) + geom_line()
+# 3. bmi 평균 데이터로 라인 그래프 그리기
+ggplot(data=bmi_mean, aes(x=age, y=bmi)) + geom_line()
